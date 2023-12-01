76f993aac6866feccf5b982a487c6c58f66ba861
Huaqi Fang <578567190@qq.com>
2023-12-01 18:16:51 +0800

    ci: Fix deploy issue report by mkdocs
    see https://github.com/Nuclei-Software/nuclei-studio/actions/runs/7058577032/job/19214440581#step:6:14
    INFO    -  Doc file 'README.md' contains a link to '0-template.md' which is excluded from the built site.
    
    Signed-off-by: Huaqi Fang <578567190@qq.com>

--- .github/prepare_doc.sh
+++ .github/prepare_doc.sh
@@ -12,6 +12,8 @@ mkdir -p docs
 cp -rf *.md docs/
 cp -rf asserts/ docs/
 
+sed -i "s|0-template.md|https://github.com/Nuclei-Software/nuclei-studio/blob/main/0-template.md|" docs/README.md
+
 echo "Document for mkdocs is prepared!"
 echo "Run the following command to build and serve these doc!"
 echo "mkdocs serve -a 0.0.0.0:8009"
